caf7e48e76d1fe91b8057459d33e0d3dec8d924c
Peleg <benbapel@post.bgu.ac.il>
2022-04-19 21:55:44 +0300

    Created User class and UserTest, structured the solution as instructed

---
+++ BackendTests/UserTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.BackendTests.UserTest
+{
+    public class UserTest
+    {
+        /// <summary>
+        /// This method tests a valid registration of a new user to the system.
+        /// </summary>
+        /// <param name="email">The user email address, used as the username for logging the system.</param>
+        /// <param name="password">The user password.</param>
+        /// <returns>The string "{}", unless an error occurs (see <see cref="GradingService"/>)</returns>
+        public string Register(string email, string password)
+        {
+            Backend.ServiceLayer.User.createUser(email, password);
+            return "{}";
+        }
+    }
+}
