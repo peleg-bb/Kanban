9f2b6c7f1ec41519014a00422b0ce99630151634
Itay <Itayab@post.bgu.ac.il>
2022-04-20 14:05:15 +0300

    Updated the docstring for each function and test function

--- Backend/ServiceLayer/Task.cs
+++ Backend/ServiceLayer/Task.cs
@@ -8,29 +8,58 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
 {
     public class Task 
     {
-
-        public string createTask(string title,string description, string dueDate)
+        /// <summary>
+        /// This method creates a new task.
+        /// </summary>
+        /// <param name="email">Email of the user. The user must be logged in.</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="title">Title of the new task</param>
+        /// <param name="description">Description of the new task</param>
+        /// <param name="dueDate">The due date if the new task</param>
+        /// <returns>Response with user-email, unless an error occurs (see <see cref="GradingService"/>)</returns>
+        public string createTask(string email, string boardName, string title,string description, string dueDate)
         {
-            string json = "Not Implemented Yet";
-            return json;
+            throw new NotImplementedException();
+            
         }
-        public string editTitle(string newTitle, string userMail, string boardName, string title)
+        /// <summary>
+        /// This method updates task title.
+        /// </summary>
+        /// <param name="email">Email of user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="taskId">The task to be updated identified task ID</param>
+        /// <param name="newTitle">New title for the task</param>
+        /// <returns>The string "{\"Title\" : \"newTitle\", \"Description\" : \"description\", \"DueDate\" : \"21.04.22\"}", unless an error occurs (see <see cref="GradingService"/>)</returns>
+        public string editTitle(string newTitle, string email, string boardName, string taskId)
         {
 
-            string json = "Not Implemented Yet";
-            return json;
+            throw new NotImplementedException();
 
         }
-        public string editDescription(string newDescription, string userMail, string boardName, string title)
+        /// <summary>
+        /// This method updates the description of a task.
+        /// </summary>
+        /// <param name="email">Email of user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="taskId">The task to be updated identified task ID</param>
+        /// <param name="newDescription">New description for the task</param>
+        /// <returns>The string "{\"Title\" : \"title\", \"Description\" : \"newDescription\", \"DueDate\" : \"21.04.22\"}", unless an error occurs (see <see cref="GradingService"/>)</returns>
+        public string editDescription(string newDescription, string email, string boardName, string taskId)
         {
-            string json = "Not Implemented Yet";
-            return json;
+            throw new NotImplementedException();
 
         }
-        public string editDueDate(string newDueDate, string userMail, string boardName, string title)
+        /// <summary>
+        /// This method updates the due date of a task
+        /// </summary>
+        /// <param name="email">Email of the user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="taskId">The task to be updated identified task ID</param>
+        /// <param name="newDueDate">The new due date of the column</param>
+        /// <returns>The string "{\"Title\" : \"title\", \"Description\" : \"description\", \"DueDate\" : \"newDueDate"}", unless an error occurs (see <see cref="GradingService"/>)</returns>
+        public string editDueDate(string newDueDate, string email, string boardName, string taskId)
         {
-            string json = "Not Implemented Yet";
-            return json;
+            throw new NotImplementedException();
 
         }
     }
--- BackendTests/ServiceLayer/TaskTests.cs
+++ BackendTests/ServiceLayer/TaskTests.cs
@@ -12,6 +12,10 @@ namespace BackendTests.ServiceLayer
     internal class TaskTests
 
     {
+        /// <summary>
+        /// This method test if we can create a new task and we get it in the JSON format
+        /// for requirement 17 and requirement 4
+        /// </summary>
         [TestMethod]
         public void test1()
         {
@@ -20,48 +24,63 @@ namespace BackendTests.ServiceLayer
             string duedate = "21.04.22";
             Task task = new Task();
             //act
-            Assert.Equals(task.createTask(title, description, duedate),
+            Assert.Equals(task.createTask('Itay@gmail.com',"To Do", title, description, duedate),
                 "{\"Title\" : \"New Task\", \"Description\" : \"this is check for new task\", \"DueDate\" : \"21.04.22\"}"
             );
         }
+        /// <summary>
+        /// This method test if we can edit the title of task and we get it in the JSON format
+        /// for requirement 17 and requirement 14,15
+        /// </summary>
         [TestMethod]
         public void test2()
         {
             string title = "New Task";
             string description = "this is check for new task";
             string duedate = "21.04.22";
+            string taskid = "123";
             Task task = new Task();
             string newTitle = "new title";
             //act
-            string jsonup = task.editTitle(newTitle, "Itay@gmail.com", "ToDo", title);
+            string jsonup = task.editTitle(newTitle, "Itay@gmail.com", "ToDo", taskid);
             Assert.Equals(jsonup,
                 "{\"Title\" : \"new title\", \"Description\" : \"this is check for new task\", \"DueDate\" : \"21.04.22\"}"
             );
         }
+        /// <summary>
+        /// This method test if we can edit the description of task and we get it in the JSON format
+        /// for requirement 17 and requirement 14,15
+        /// </summary>
         [TestMethod]
         public void test3()
         {
             string title = "New Task";
             string description = "this is check for new task";
             string duedate = "21.04.22";
+            string taskid = "123";
             Task task = new Task();
             string newDescription = "Does it change?";
             //act
-            string jsonup = task.editDescription(newDescription, "Itay@gmail.com", "ToDo", title);
+            string jsonup = task.editDescription(newDescription, "Itay@gmail.com", "ToDo", taskid);
             Assert.Equals(jsonup,
                 "{\"Title\" : \"New Task\", \"Description\" : \"Does it change?\", \"DueDate\" : \"21.04.22\"}"
             );
         }
+        /// <summary>
+        /// This method test if we can edit the due date of task and we get it in the JSON format
+        /// for requirement 17 and requirement 14,15
+        /// </summary>
         [TestMethod]
         public void test4()
         {
             string title = "New Task";
             string description = "this is check for new task";
             string duedate = "21.04.22";
+            string taskid = "123";
             Task task = new Task();
             string newDueDate = "22.04.22";
             //act
-            string jsonup = task.editDueDate(newDueDate, "Itay@gmail.com", "ToDo", title);
+            string jsonup = task.editDueDate(newDueDate, "Itay@gmail.com", "ToDo", taskid);
             Assert.Equals(jsonup,
                 "{\"Title\" : \"New Task\", \"Description\" : \"this is check for new task\", \"DueDate\" : \"22.04.22\"}"
             );
