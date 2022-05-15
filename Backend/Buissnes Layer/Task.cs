37db371aa019041928ba2a3d0b4fa62895618f48
Itay <Itayab@post.bgu.ac.il>
2022-05-15 17:41:39 +0300

    new Bussnes layer Task

--- Backend/Buissnes Layer/Task.cs
+++ Backend/Buissnes Layer/Task.cs
@@ -8,6 +8,40 @@ namespace IntroSE.Kanban.Backend.Buissnes_Layer
 {
     public class Task
     {
+        private string Title { set; get; }
 
+        private string Description { set; get; }
+        private string DueDate { set; get; }
+        public readonly string CreationDate = DateTime.Now.ToString("'yyyy'-'MM'-'dd'");
+        private int State = 0;
+        private int TaskId { get; }
+        private int ID = 0;
+
+        public Task (string title, string description, string dueDate, int state)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.DueDate = dueDate;
+            this.CreationDate = DateTime.Now.ToString("'yyyy'-'MM'-'dd'");
+            this.State = state;
+            this.TaskId = ID;
+            ID += 1;
+
+        }
+        private void EditTitle(string newTitle)
+        {
+            this.Title = newTitle;
+
+        }
+
+        private void EditDescription(string newDescription)
+        {
+            this.Description = newDescription;
+        }
+
+        private void EditDueDate(string newDueDate)
+        {
+            this.DueDate = newDueDate;
+        }
     }
 }
