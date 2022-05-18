368941e4c12b7b49a4bae1005788467bb39b3b63
Itay <Itayab@post.bgu.ac.il>
2022-05-18 18:45:28 +0300

    added some tests

--- Backend/Buissnes Layer/UserController.cs
+++ Backend/Buissnes Layer/UserController.cs
@@ -42,21 +42,9 @@ namespace IntroSE.Kanban.Backend.Buissnes_Layer
         }
         public bool IsValidEmail(string email)
         {
-            var trimmedEmail = email.Trim();
-
-                if (trimmedEmail.EndsWith("."))
-                {
-                    return false; // suggested by @TK-421
-                }
-                try
-                {
-                    var addr = new System.Net.Mail.MailAddress(email);
-                    return addr.Address == trimmedEmail;
-                }
-                catch
-                {
-                    return false;
-                }
+            Regex regex = new Regex(
+                @"^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z](-?[a-zA-Z0-9])*(\.[a-zA-Z](-?[a-zA-Z0-9])*)+$");
+            return regex.IsMatch(email);
         }
 
         public void CreateUser(string email, string password)
