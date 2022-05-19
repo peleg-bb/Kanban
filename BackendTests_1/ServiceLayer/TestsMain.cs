c4de637b49a4d5b1a78bd9a3cd68cb51066f2494
ptamar <ptamar@post.bgu.ac.il>
2022-05-19 14:11:16 +0300

    last changesss44

--- BackendTests_1/ServiceLayer/TestsMain.cs
+++ BackendTests_1/ServiceLayer/TestsMain.cs
@@ -153,28 +153,28 @@ namespace BackendTests.ServiceLayer
 
 
 
-            BoardTest boraTest = new BoardTest(boardService);
-            boraTest.ValidCreateBoardTest();
-            boraTest.InvalidCreateBoardTest();
-            boraTest.InvalidCreateBoardTest2();
-            boraTest.AddValidTaskTest();
-            boraTest.AddInvalidTaskTest2();
-            boraTest.ValidNextStateTest();
-            boraTest.InvalidNextStateTest();
-            boraTest.InvalidNextStateTest2();
-            boraTest.InvalidNextStateTest3();
-            boraTest.ValidDeleteBoardTest();
-            boraTest.InvalidDeleteBoardTest();
-            boraTest.ValidGetColum();
-            boraTest.InvalidGetColum();
-            boraTest.ValidGetColumnLimit();
-            boraTest.InvalidGetColumnLimit();
-            boraTest.ValidGetColumnName();
-            boraTest.InvalidGetColumnName();
-            boraTest.ValidInProgress();
-            boraTest.InvalidInProgress();
-            boraTest.ValidLimitColumn();
-            boraTest.InvalidLimitColumn();
+            //BoardTest boraTest = new BoardTest(boardService);
+            //boraTest.ValidCreateBoardTest();
+            //boraTest.InvalidCreateBoardTest();
+            //boraTest.InvalidCreateBoardTest2();
+            //boraTest.AddValidTaskTest();
+            //boraTest.AddInvalidTaskTest2();
+            //boraTest.ValidNextStateTest();
+            //boraTest.InvalidNextStateTest();
+            //boraTest.InvalidNextStateTest2();
+            //boraTest.InvalidNextStateTest3();
+            //boraTest.ValidDeleteBoardTest();
+            //boraTest.InvalidDeleteBoardTest();
+            //boraTest.ValidGetColum();
+            //boraTest.InvalidGetColum();
+            //boraTest.ValidGetColumnLimit();
+            //boraTest.InvalidGetColumnLimit();
+            //boraTest.ValidGetColumnName();
+            //boraTest.InvalidGetColumnName();
+            //boraTest.ValidInProgress();
+            //boraTest.InvalidInProgress();
+            //boraTest.ValidLimitColumn();
+            //boraTest.InvalidLimitColumn();
 
             Console.WriteLine("bye");
 
@@ -190,55 +190,79 @@ namespace BackendTests.ServiceLayer
             //userTests.invalidUserCreation_2();
             //userTests.logoutTest();
             //userTests.invalidLogoutTest();
-           // Console.WriteLine("hey again");
-           // GradingService gradingService = new GradingService();
-           // string emailll = "whyy@gmail.com";
-           // string board1 = "first";
-           // gradingService.Register(emailll, "Aka123k123");
-           // gradingService.Login(emailll, "Aka123k123");
-           // string invalid1 = "jgiosejiooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooojjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
-           // Console.WriteLine(gradingService.AddBoard(emailll, board1));
-           // Console.WriteLine(gradingService.AddBoard("sds", "ads"));//suppose throw user error
-           // Console.WriteLine(gradingService.AddTask(emailll, board1, "bRAND", "HELLOW WORLD", DateTime.Now));
-           // Console.WriteLine(gradingService.AddTask(emailll, board1, "new", "HELLOW WORLD", DateTime.Now));
-           // Console.WriteLine(gradingService.AddTask("ddd", board1, "new", "HELLOW WORLD", DateTime.Now));//suppose throw user error
-           // Console.WriteLine(gradingService.AddBoard(emailll, board1)); //supose to throw board exception
-           // Console.WriteLine("NOW I AM CHECKING ADVANCE TASK!!");
-           // Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 0, 0));
-           // Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 1, 0));
-           // Console.WriteLine(gradingService.AdvanceTask("fff", board1, 0, 0));//suppose throw user error
-           // Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 2, 0));// at Done
-           // Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 0, 1));
-           // Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 0, 1));//dose not exist
-           //// Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 1, 1));
-           // Console.WriteLine(gradingService.AddTask(emailll, board1, "new", "HELLOW WORLD", DateTime.Now)); // no such task in column 0
-           // Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 0, 2));
-           // Console.WriteLine("NOW I AM InProgress TASK!!");
-           // Console.WriteLine(gradingService.InProgressTasks(emailll));
-           // Console.WriteLine(gradingService.InProgressTasks("sss"));
-           // Console.WriteLine("NOW I AM LimitColumn TASK!!");
-           // Console.WriteLine(gradingService.LimitColumn(emailll, board1, 1, 5));//valid
-           // Console.WriteLine(gradingService.LimitColumn(emailll, board1, 1, 4));//Valid
-           // Console.WriteLine(gradingService.LimitColumn(emailll, board1, 1, -1));//Valid
-           // Console.WriteLine(gradingService.LimitColumn(emailll, board1, 2, 0));//InValid amount
-           // Console.WriteLine(gradingService.LimitColumn(emailll, board1, 4, 10));// invalid column
-           // Console.WriteLine(gradingService.LimitColumn(emailll, "ddd", 2, 10));// invalid board
-           // Console.WriteLine(gradingService.LimitColumn("ssds", board1, 1, 10));//user does not exist
-           // Console.WriteLine("NOW I AM GetColumnLimit TASK!!");
-           // Console.WriteLine(gradingService.GetColumnLimit("emailll", board1, 1));
-           // Console.WriteLine(gradingService.GetColumnLimit(emailll, board1, 4));
-           // Console.WriteLine(gradingService.GetColumnLimit(emailll, "fff", 1));
-           // Console.WriteLine(gradingService.GetColumnLimit(emailll, board1, 1));
-           // Console.WriteLine("NOW I AM GetColumnName TASK!!");
-           // Console.WriteLine(gradingService.GetColumnName(emailll, board1, 5)); // INVALID NUMBER
-           // Console.WriteLine(gradingService.GetColumnName("ds", board1, 2)); // INVALID mail
-           // Console.WriteLine(gradingService.GetColumnName(emailll, "njjj", 2)); // INVALID board
-           // Console.WriteLine(gradingService.GetColumnName(emailll, board1, 2)); // VALID
-           // Console.WriteLine("NOW I AM RemoveBoard TASK!!");
-           // Console.WriteLine(gradingService.RemoveBoard("ff", board1));// INVALID mail
-           // Console.WriteLine(gradingService.RemoveBoard(emailll, "fff"));// INVALID board
-           // Console.WriteLine(gradingService.RemoveBoard(emailll, board1));// VALID 
-           // Console.WriteLine("that it for now !!!!");
+            // Console.WriteLine("hey again");
+            GradingService gradingService = new GradingService();
+            string emailll = "whyy@gmail.com";
+            string board1 = "first";
+            gradingService.Register(emailll, "Aka123k123");
+            gradingService.Login(emailll, "Aka123k123");
+            string invalid1 = "jgiosejiooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooojjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
+            Console.WriteLine(gradingService.AddBoard(emailll, board1));
+            Console.WriteLine(gradingService.AddBoard(null, board1));
+            Console.WriteLine("NOW email null TASK!!");
+            Console.WriteLine(gradingService.AddBoard(emailll, null));
+            Console.WriteLine("NOW boardName null TASK!!");
+            Console.WriteLine(gradingService.AddBoard("sds", "ads"));//suppose throw user error
+            Console.WriteLine(gradingService.AddTask(emailll, board1, "bRAND", "HELLOW WORLD", DateTime.Now));
+            Console.WriteLine(gradingService.AddTask(emailll, board1, "new", "HELLOW WORLD", DateTime.Now));
+            Console.WriteLine(gradingService.AddTask("ddd", board1, "new", "HELLOW WORLD", DateTime.Now));//suppose throw user error
+            Console.WriteLine(gradingService.AddBoard(emailll, board1)); //supose to throw board exception
+            Console.WriteLine("from NOW all  null TASK!!");
+
+            Console.WriteLine(gradingService.AddTask(null, board1, "bRAND", "HELLOW WORLD", DateTime.Now));
+            Console.WriteLine(gradingService.AddTask(emailll, null, "bRAND", "HELLOW WORLD", DateTime.Now));
+            Console.WriteLine(gradingService.AddTask(emailll, board1, null, "HELLOW WORLD", DateTime.Now));
+            Console.WriteLine(gradingService.AddTask(emailll, board1, "bRAND", null, DateTime.Now));
+
+            Console.WriteLine("NOW I AM CHECKING ADVANCE TASK!!");
+            Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 0, 0));
+            Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 1, 0));
+            Console.WriteLine(gradingService.AdvanceTask("fff", board1, 0, 0));//suppose throw user error
+            Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 2, 0));// at Done
+            Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 0, 1));
+            Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 0, 1));//dose not exist
+            Console.WriteLine("from NOW all  null TASK!!");
+
+            Console.WriteLine(gradingService.AdvanceTask(null, board1, 0, 0));//null
+            Console.WriteLine(gradingService.AdvanceTask(emailll, null, 0, 0));
+            Console.WriteLine("end all  null TASK!!");
+
+            Console.WriteLine(gradingService.AddTask(emailll, board1, "new", "HELLOW WORLD", DateTime.Now)); // no such task in column 0
+            Console.WriteLine(gradingService.AdvanceTask(emailll, board1, 0, 2));
+            Console.WriteLine("NOW I AM InProgress TASK!!");
+
+            Console.WriteLine(gradingService.InProgressTasks(null));
+
+            Console.WriteLine(gradingService.InProgressTasks(emailll));
+            Console.WriteLine(gradingService.InProgressTasks("sss"));
+            Console.WriteLine("NOW I AM LimitColumn TASK!!");
+            Console.WriteLine(gradingService.LimitColumn(emailll, board1, 1, 5));//valid
+            Console.WriteLine(gradingService.LimitColumn(emailll, board1, 1, 4));//Valid
+            Console.WriteLine(gradingService.LimitColumn(emailll, board1, 1, -1));//Valid
+            Console.WriteLine(gradingService.LimitColumn(emailll, board1, 2, 0));//InValid amount
+            Console.WriteLine(gradingService.LimitColumn(emailll, board1, 4, 10));// invalid column
+            Console.WriteLine(gradingService.LimitColumn(emailll, "ddd", 2, 10));// invalid board
+            Console.WriteLine(gradingService.LimitColumn(emailll, null, 2, 10));// invalid board
+            Console.WriteLine(gradingService.LimitColumn("ssds", board1, 1, 10));//user does not exist
+            //Nullable<int> i = null;
+         
+            Console.WriteLine(gradingService.LimitColumn(emailll, board1, 1, 4));//Valid
+
+            Console.WriteLine("NOW I AM GetColumnLimit TASK!!");
+            Console.WriteLine(gradingService.GetColumnLimit("emailll", board1, 1));
+            Console.WriteLine(gradingService.GetColumnLimit(emailll, board1, 4));
+            Console.WriteLine(gradingService.GetColumnLimit(emailll, "fff", 1));
+            Console.WriteLine(gradingService.GetColumnLimit(emailll, board1, 1));
+            Console.WriteLine("NOW I AM GetColumnName TASK!!");
+            Console.WriteLine(gradingService.GetColumnName(emailll, board1, 5)); // INVALID NUMBER
+            Console.WriteLine(gradingService.GetColumnName("ds", board1, 2)); // INVALID mail
+            Console.WriteLine(gradingService.GetColumnName(emailll, "njjj", 2)); // INVALID board
+            Console.WriteLine(gradingService.GetColumnName(emailll, board1, 2)); // VALID
+            Console.WriteLine("NOW I AM RemoveBoard TASK!!");
+            Console.WriteLine(gradingService.RemoveBoard("ff", board1));// INVALID mail
+            Console.WriteLine(gradingService.RemoveBoard(emailll, "fff"));// INVALID board
+            Console.WriteLine(gradingService.RemoveBoard(emailll, board1));// VALID 
+            Console.WriteLine("that it for now !!!!");
 
 
 
