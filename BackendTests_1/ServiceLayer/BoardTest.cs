a7bbdb6639befa53bcd19bb6ced106452e31716b
Peleg <benbapel@post.bgu.ac.il>
2022-05-19 21:47:40 +0300

    last test changes

--- BackendTests_1/ServiceLayer/BoardTest.cs
+++ BackendTests_1/ServiceLayer/BoardTest.cs
@@ -28,7 +28,7 @@ namespace BackendTests.ServiceLayer
             Response r = new Response(null, true);
             string email = "tamar@gmail.com";
             string boardName = "test2";
-            Assert.AreEqual(_boardService.CreateBoard(boardName, email), "{}");
+            Assert.AreEqual(_boardService.CreateBoard(boardName, email), r.OKJson());
 
         }
 
