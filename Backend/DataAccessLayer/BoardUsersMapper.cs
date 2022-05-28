fde634f88c6e9e3f4831dfb4f62df86b8cf29515
Peleg <benbapel@post.bgu.ac.il>
2022-05-28 12:54:03 +0300

    Created all classes in Data Access Layer according to the design. Wrote the beginning of some methods in User and BoardUsers part of the layer.

---
+++ Backend/DataAccessLayer/BoardUsersMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.DataAccessLayer
+{
+    internal class BoardUsersMapper
+    {
+        private List<BoardUsersDTO> _boards;
+
+        public void CreateBoard(int boardID, string ownerEmail)
+        {
+            _boards.Add(new BoardUsersDTO(boardID, ownerEmail));
+        }
+
+        public void DeleteBoard(int boardID)
+        {
+            _boards.RemoveAll(itemCollection => boardID == itemCollection.BoardID);
+            // Note that this syntax represents a predicate lambda function as studied in the Principles of OOP course.
+        }
+
+        public void AddUser(int boardID, string userEmail)
+        {
+            _boards.Add(new BoardUsersDTO(boardID, userEmail));
+        }
+
+        public void RemoveUser(int boardID, string userEmail)
+        {
+            _boards.RemoveAll(item => (item.BoardID == boardID && item.userName == userEmail));
+        }
+    }
+}
